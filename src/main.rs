c43f57853fe6e8a72b7a40b35d3c4f67bf0b2a59
Dr-MonkyBrainF <wiiu.daisuki321@gmail.com>
2025-07-27 04:26:35 +0900

    first commit

---
+++ src/main.rs
@@ -0,0 +1,85 @@
+use bevy::prelude::*;
+use rand::{Rng, thread_rng};
+
+fn main() {
+    App::new()
+        .add_plugins(DefaultPlugins.set(ImagePlugin::default_nearest()))
+        .add_systems(Startup, setup)
+        .add_systems(Update, rps_system)
+        .run();
+}
+
+#[derive(Component, Clone, Copy, PartialEq, Eq, Debug)]
+pub enum Hand {
+    Rock,
+    Scissors,
+    Paper,
+}
+
+// コンポーネントでどの手かを記録
+#[derive(Component)]
+pub struct RspButton(Hand);
+
+//画像を並べる
+pub fn setup(mut commands: Commands, asset_server: Res<AssetServer>) {
+    commands.spawn(Camera2d);
+
+    commands
+        .spawn(Node {
+            width: Val::Percent(100.),
+            height: Val::Percent(100.),
+            justify_content: JustifyContent::Center,
+            align_items: AlignItems::Center,
+            flex_direction: FlexDirection::Row,
+            column_gap: Val::Px(20.),
+            ..default()
+        })
+        .with_children(|parent| {
+            for (hand, path) in [
+                (Hand::Rock, "rps/rock.png"),
+                (Hand::Scissors, "rps/scissors.png"),
+                (Hand::Paper, "rps/paper.png"),
+            ] {
+                parent.spawn((
+                    ImageNode::from(asset_server.load(path)),
+                    Node {
+                        //position_type: PositionType::Absolute,
+                        width: Val::Px(500.),
+                        height: Val::Px(500.),
+                        ..default()
+                    },
+                    Interaction::None,
+                    RspButton(hand),
+                ));
+            }
+        });
+}
+
+//CPUのランダムな手
+pub fn random_hand() -> Hand {
+    match thread_rng().gen_range(0..=2) {
+        0 => Hand::Rock,
+        1 => Hand::Paper,
+        _ => Hand::Scissors,
+    }
+}
+
+//クリックを検知しランダムを呼び出し勝敗をつける
+pub fn rps_system(mut query: Query<(&Interaction, &RspButton), Changed<Interaction>>) {
+    let cpu_hand = random_hand();
+    for (interaction, rps_button) in &mut query {
+        if *interaction == Interaction::Pressed {
+            let player_hand = rps_button.0;
+
+            println!("『あなた: {player_hand:?}』 VS 『CPU: {cpu_hand:?}』");
+
+            match (player_hand, cpu_hand) {
+                (a, b) if a == b => println!("引き分け！"),
+                (Hand::Rock, Hand::Scissors)
+                | (Hand::Paper, Hand::Rock)
+                | (Hand::Scissors, Hand::Paper) => println!("あなたの勝ち！"),
+                _ => println!("あなたの負け！"),
+            }
+        }
+    }
+}
